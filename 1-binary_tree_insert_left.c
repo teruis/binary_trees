7293342e8489b8511c7965eca3aeb058fdf2d1b3
teruis <126614240+teruis@users.noreply.github.com>
2023-11-29 16:57:06 +0100

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,20 +10,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
-        return NULL;
+	binary_tree_t *new_node;
 
-    binary_tree_t *new_node = binary_tree_node(parent, value);
+	if (parent == NULL)
+		return (NULL);
 
-    if (new_node == NULL)
-        return NULL;
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
 
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-        new_node->left->parent = new_node;
-    }
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
 
-    parent->left = new_node;
-    return new_node;
+	return (new_node);
 }
