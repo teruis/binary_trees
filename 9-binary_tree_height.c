0775a6e936d1e5eb9ae2baeddfe2d974e277f288
teruis <mohamadelassouli2002@gmail.com>
2023-11-30 22:25:15 +0100

    x

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,17 +8,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+    size_t left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return 0;
 
-	if (tree->left)
-		left_height = 1 + binary_tree_height(tree->left);
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
 
-	if (tree->right)
-		right_height = 1 + binary_tree_height(tree->right);
-
-	return ((left_height > right_height) ? left_height : right_height);
+    if (left_height > right_height)
+        return (left_height + 1);
+    else
+        return (right_height + 1);
 }
