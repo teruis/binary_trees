d52fa1f4d1e28ca52680c169d19505f64583f39a
teruis <126614240+teruis@users.noreply.github.com>
2023-11-30 22:19:48 +0100

    Update 9-binary_tree_height.c
    x

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
-    if (tree == NULL)
-        return 0;
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
 
-    if (left_height > right_height)
-        return (left_height + 1);
-    else
-        return (right_height + 1);
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	return ((left_height > right_height) ? left_height : right_height);
 }
