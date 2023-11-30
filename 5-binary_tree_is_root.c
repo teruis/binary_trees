7b0580c337fff0410bace75435d4b79aa65fe6a7
teruis <126614240+teruis@users.noreply.github.com>
2023-11-30 21:47:51 +0100

    Update 5-binary_tree_is_root.c
    x

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,8 +8,8 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node)
-        return 0;
+	if (node && !node->parent)
+		return (1);
 
-    return (node->parent) ? 1 : 0;
+	return (0);
 }
